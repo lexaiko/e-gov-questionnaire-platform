d75d8b70c1e8a9b5b92cfd32e47a3cfb0addbf6c
Lexaiko <ekosusantobagussusanto@gmail.com>
2025-08-03 14:51:20 +0000

    feat: Remove Google authentication configuration and related view components, remove must verify on User model

--- resources/views/filament/pages/login.blade.php
+++ resources/views/filament/pages/login.blade.php
@@ -14,6 +14,4 @@
 
         <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
     </x-filament-panels::form>
-
-    {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
 </x-filament-panels::page.simple>
